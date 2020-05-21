90d5bf7566ae08bd4358ade6a3a6ba628b0e88d8
Tarun Kumar <kumar.tarun097@gmail.com>
2020-05-21 14:34:47 +0530

    Add project files.

---
+++ AccessDataBase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+
+namespace AccessDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "company";
+
+            if (dbCon.IsConnect())
+            {
+                try
+                {
+                    string query = "SELECT firstname,lastname FROM employee";
+                    var cmd = new MySqlCommand(query, dbCon.Connection);
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string someStringFromColumnZero = reader.GetString(0);
+                        string someStringFromColumnOne = reader.GetString(1);
+                        Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    dbCon.Close();
+                }
+            }
+
+        }
+    }
+}
