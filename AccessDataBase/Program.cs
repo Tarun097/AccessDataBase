6837cdaa777a9779081222183c7fe230e6fa7f8e
Tarun Kumar <kumar.tarun097@gmail.com>
2020-05-28 14:22:07 +0530

    Disposed reader, moved dbConnection close call in finally

--- AccessDataBase/Program.cs
+++ AccessDataBase/Program.cs
@@ -17,15 +17,21 @@ namespace AccessDataBase
                 {
                     string query = "SELECT firstname,lastname FROM employee";
                     var cmd = new MySqlCommand(query, dbCon.Connection);
-                    var reader = cmd.ExecuteReader();
-                    while (reader.Read())
+                    using (var reader = cmd.ExecuteReader())
                     {
-                        string someStringFromColumnZero = reader.GetString(0);
-                        string someStringFromColumnOne = reader.GetString(1);
-                        Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                        while (reader.Read())
+                        {
+                            string someStringFromColumnZero = reader.GetString(0);
+                            string someStringFromColumnOne = reader.GetString(1);
+                            Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                        }
                     }
                 }
                 catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception {ex.Message}");
+                }
+                finally 
                 {
                     dbCon.Close();
                 }
